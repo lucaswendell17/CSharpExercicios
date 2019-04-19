bcdc7ddc61f55a75f81a1c60df92f4d4ba7483af
Lucas Wendel <lucas.wmartins@hotmail.com>
2019-04-19 17:07:02 -0300

    Commit Static

---
+++ ExMetodoStatic/Calculadora.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ExMetodoStatic{
+    class Calculadora{
+         public double Pi = 3.14;
+         public double Circunferencia(double r){
+            return 2.0 * Pi * r;
+        }
+        public double Volume(double r){
+            return 4.0 / 3.0 * Pi * Math.Pow(r, 3);
+        }
+    }
+}
---
+++ ExMetodoStatic/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace ExMetodoStatic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calc = new Calculadora();
+            Console.Write("Entre o valor do raio: ");
+            double raio = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            
+            double circ = calc.Circunferencia(raio);
+            double volume = calc.Volume(raio);
+
+            System.Console.WriteLine("Circunferência: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            System.Console.WriteLine("Volume: " + volume.ToString("F2", CultureInfo.InvariantCulture));
+            System.Console.WriteLine("Valor de PI: " + calc.Pi.ToString("F2", CultureInfo.InvariantCulture));
+        }
+        
+    }
+}
