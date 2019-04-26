54613cf14d7d7d6913022bde6e67f08cbaf334e0
Lucas Wendel <lucas.wmartins@hotmail.com>
2019-04-26 19:32:44 -0300

    Ex Aluguel

---
+++ ExAluguel/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ExAluguel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Estudante[] vect = new Estudante[10];
+
+            Console.Write("Quantos quartos serão alugados?  ");
+            int quarto = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= quarto; i++)
+            {
+                System.Console.WriteLine("Aluguel #"+i);
+                System.Console.Write("Nome: ");
+                string nome = Console.ReadLine();
+                System.Console.Write("Email: ");
+                string email = Console.ReadLine();
+                System.Console.Write("Quarto: ");
+                int qto = int.Parse(Console.ReadLine());
+                vect[qto] = new Estudante(nome, email);
+            }
+
+            System.Console.WriteLine("Quartos Ocupados: ");
+            System.Console.WriteLine();
+
+            for (int i = 0; i < vect.Length; i++)
+            {
+                if (vect[i] != null)
+                    System.Console.WriteLine(i + ": " + vect[i]);
+            }
+
+        }
+    }
+}
