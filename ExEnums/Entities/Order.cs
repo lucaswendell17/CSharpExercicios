8b0f0a9567800f3f1d61baeae4c2aaeab992c7e5
Lucas Wendel <lucas.wmartins@hotmail.com>
2019-05-30 17:59:24 -0300

    Exercicio Enums

---
+++ ExEnums/Entities/Order.cs
@@ -0,0 +1,19 @@
+using ExEnums.Entities.Enums;
+using System;
+namespace ExEnums.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public DateTime Moment { get; set; }
+        public OrderStatus Status { get; set; }
+
+        public override string ToString(){
+            return Id
+            + ", "
+            +Moment
+            +", "
+            +Status;
+        }
+    }
+}
---
+++ ExEnums/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using ExEnums.Entities;
+using ExEnums.Entities.Enums;
+
+namespace ExEnums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Order order = new Order{
+                Id = 1080,
+                Moment = DateTime.Now,
+                Status = OrderStatus.PendingPayment
+            };
+            System.Console.WriteLine(order);
+
+            string txt = OrderStatus.PendingPayment.ToString();
+
+            OrderStatus os = Enum.Parse<OrderStatus>("Delivered");
+
+            System.Console.WriteLine(os);
+            System.Console.WriteLine(txt);
+        }
+    }
+}
